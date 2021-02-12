195ebd2195a1691e425aa20d40b4d073933d7753
Peter Mulikov <pettergov1@gmail.com>
2021-02-12 14:43:20 +0100

    Create class Draw

---
+++ One-Armed-Bandit-OPP/Draw.js
@@ -0,0 +1,27 @@
+'use strict';
+
+// Create class Draw
+class Draw {
+  constructor() {
+    this.options = ['red', 'green', 'blue'];
+
+    // Private variable
+    let _result = this.drawResult();
+
+    // Method for get result of draw
+    this.getDrawResult = () => _result;
+  }
+
+  drawResult() {
+    const colors = [];
+
+    this.options.forEach(() => {
+      const index = Math.floor(Math.random() * this.options.length);
+
+      const color = this.options[index];
+      colors.push(color);
+    });
+
+    return colors;
+  }
+}
