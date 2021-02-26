31a579b6545e49cb55bfd9769d7ddd5e4ebbbc92
Peter Mulikov <pettergov1@gmail.com>
2021-02-26 18:41:18 +0100

    Create method add item to data structure

--- Wishes Keeper/script.js
+++ Wishes Keeper/script.js
@@ -14,8 +14,8 @@ const ItemController = (function () {
   const data = {
     items: [
       { id: 0, name: 'iPhone-12', cost: 1000 },
-      { id: 0, name: 'Laptop', cost: 1400 },
-      { id: 0, name: 'Car (BMW-i3)', cost: 8900 },
+      { id: 1, name: 'Laptop', cost: 1400 },
+      { id: 2, name: 'Car (BMW-i3)', cost: 8900 },
     ],
     currentItem: null,
     totalCost: 0,
@@ -26,7 +26,25 @@ const ItemController = (function () {
     getItems: function () {
       return data.items;
     },
+    addItem: function (name, cost) {
+      let id;
+      // Create id
+      if (data.items.length > 0) {
+        id = data.items[data.items.length - 1].id + 1;
+      }
+      if (data.items.length < 0) id = 0;
 
+      // Cost to number
+      const costNumber = parseInt(cost);
+
+      // Create new item
+      const newItem = new Item(id, name, costNumber);
+
+      // Add to items array
+      data.items.push(newItem);
+
+      return newItem;
+    },
     logData: function () {
       return data;
     },
@@ -35,33 +53,70 @@ const ItemController = (function () {
 
 // UI COntroller
 const UIController = (function () {
-  const IUSelectors = {
+  const UISelectors = {
     itemList: '#item-list',
+    addBtn: '.add-btn',
+    itemNameInput: '#item-name',
+    itemCostInput: '#item-cost',
   };
+
   // Public method
   return {
     populateItemList: function (items) {
       let html = ``;
 
       items.forEach(item => {
+        const { id, name, cost } = item;
         html += `
-        <li class="list-group-item" id="item-${item.id}">
-          <strong>${item.name}:</strong> <em>${item.cost} $</em>
+        <li class="list-group-item" id="item-${id}">
+          <strong>${name}:</strong> <em>${cost} $</em>
           <a href="#" class="secondary">
             <i class="edit-item fa fa-pencil pull-right"></i>
           </a>
         </li>
         `;
       });
-
       // Insert list items
-      document.querySelector(IUSelectors.itemList).innerHTML = html;
+      document.querySelector(UISelectors.itemList).innerHTML = html;
+    },
+    getItemInput: function () {
+      return {
+        name: document.querySelector(UISelectors.itemNameInput).value,
+        cost: document.querySelector(UISelectors.itemCostInput).value,
+      };
+    },
+    getSelectors: function () {
+      return UISelectors;
     },
   };
 })();
 
 // App Controller
 const App = (function (ItemController, UIController) {
+  // Add item submit
+  const itemAddSubmit = function (e) {
+    e.preventDefault();
+    // Get form input from UI Controller
+    const input = UIController.getItemInput();
+
+    // Check for name and cost input
+    if (input.name !== '' && input.cost !== '') {
+      // Add Item
+      const newItem = ItemController.addItem(input.name, input.cost);
+    }
+  };
+
+  // Load event listeners
+  const loadEventListeners = function () {
+    // Get UI Selectors
+    const UISelectors = UIController.getSelectors();
+
+    // Add item event
+    document
+      .querySelector(UISelectors.addBtn)
+      .addEventListener('click', itemAddSubmit);
+  };
+
   // Public methods
   return {
     init: function () {
@@ -70,6 +125,9 @@ const App = (function (ItemController, UIController) {
 
       // Populate list with items
       UIController.populateItemList(items);
+
+      // Load event listeners
+      loadEventListeners();
     },
   };
 })(ItemController, UIController);
